3569fac0da68072a6ca6bc9de9bbbbebe60cdb75
John Celeste <celestejohnmark@yahoo.com>
2026-08-26 12:26:35 +0100

    Add IbookRepository and IReadOnlyBookRepository

---
+++ RiverBooks.Books/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiverBooks.Books;
+
+internal interface IBookRepository : IReadOnlyBookRepository
+{
+  Task AddAsync(Book book);
+  Task DeleteAsync(Book book);
+  Task SaveChangesAsync();
+}
