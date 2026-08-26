5a77553a0d2bbe34758c63325ae7ce3262dcb808
John Celeste <celestejohnmark@yahoo.com>
2026-08-26 12:17:16 +0100

    Add Book.cs

---
+++ RiverBooks.Books/Book.cs
@@ -0,0 +1,23 @@
+﻿namespace RiverBooks.Books
+{
+  internal class Book
+  {
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Title { get; private set; } = string .Empty;
+    public string Author { get; private set; } = string.Empty;
+    public decimal Price { get; private set; }
+
+    internal Book(Guid id, string title, string author, decimal price)
+    {
+      Id = id;
+      Title = title;
+      Author = author;
+      Price = price;
+    }
+
+    internal void UpdatePrice(decimal newPrice)
+    {
+      Price = newPrice;
+    }
+  }
+}
